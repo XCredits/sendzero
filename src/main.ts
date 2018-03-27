b75fc167085ebc12ac8d22094691f91c30b3c891
James Jansson <jamesjansson@gmail.com>
2018-03-27 23:26:02 +1100

    Move service worker registration back into the app.module.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 if (environment.production) {
@@ -12,10 +11,4 @@ if (environment.production) {
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
-    .then(() => {
-      if ('serviceWorker' in navigator) {
-        ServiceWorkerModule.register('/ngsw-worker.js', 
-            {enabled: environment.production})
-      }
-    })
     .catch(err => console.log(err));
