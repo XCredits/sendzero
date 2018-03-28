2d3ad93e826aa7c14cee22a575bfa409abcd3ce6
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-03-28 19:30:48 +1100

    Get basic chat to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,8 @@ export class AppComponent implements OnInit {
   title: string;
   peerId: string;
   peer;
+  connection;
+  messages: {text: string, sender: string}[];
   // Bind message with angular
   // message: string;
   
@@ -18,7 +20,7 @@ export class AppComponent implements OnInit {
   messageBox = null;
   receiveBox = null;
 
-  constructor() {
+  constructor(private ref: ChangeDetectorRef) {
     this.title = 'SendZero Alpha';
     // @ts-ignore
     this.peer = Peer({key: 'lwjd5qra8257b9'});
@@ -27,12 +29,23 @@ export class AppComponent implements OnInit {
       self.peerId = id;
       console.log('My Peer Id is:' + id);
     })
+    this.messages = [];
+    var self = this;
     this.peer.on('connection', function(conn) {
+      self.messageBox.disabled = false;
+      self.sendButton.disabled = false;
+      self.connection = conn;
       conn.on('data', function(data){
-        // Will print 'hi!'
         console.log(data);
+        let message = {
+          "text": data,
+          "sender": "peer"
+        }
+        self.messages.push(message);
+        self.ref.detectChanges();
       });
     });
+    
   }
 
   ngOnInit() {
@@ -44,15 +57,38 @@ export class AppComponent implements OnInit {
   }
 
   connectPeers() {
-    console.log('heerrrr');
     let peer_id = (<HTMLInputElement>document.getElementById("peer_id")).value;
     peer_id = peer_id.replace(/\s$/gi, "");
-    var conn = this.peer.connect(peer_id, {"reliable": true});
-    console.log(conn);
+    this.connection = this.peer.connect(peer_id, {"reliable": true});
+    console.log(this.connection);
     var myPeerId = this.peer.id;
-    conn.on('open', function(){
-      conn.send('hi! my peer id is ' + myPeerId);
+    var self = this;
+    this.connection.on('open', function(){
+      self.messageBox.disabled = false;
+      self.sendButton.disabled = false;
       console.log('connection is open');
     });
+    this.connection.on('data', function(data) {
+      let message = {
+        "text": data,
+        "sender": "peer"
+      }
+      self.messages.push(message);
+      self.ref.detectChanges();
+    });
+  }
+
+  sendMessage(text) {
+    if (this.peer.disconnected) {
+      alert('Not connected to peer!')
+      return;
+    }
+    let message = {
+      text: text,
+      "sender": "you"
+    }
+    console.log(this);
+    this.messages.push(message);
+    this.connection.send(text);
   }
 }
