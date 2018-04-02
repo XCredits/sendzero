ecbfd68b8abfc590d26bb357b3e70b7682120ee2
James Jansson <jamesjansson@gmail.com>
2018-04-02 16:49:20 +1000

    Show and hide navigation sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('navigationDrawer') navigationDrawer;
   title = 'app';
+  screenWidth: number;
+  sideBarMode: string;
+
+
+  toggleNavigationDrawer() {
+    this.navigationDrawer.toggle();
+  }
+
+  setSideBar() {
+    console.log(this.navigationDrawer);
+    if (this.screenWidth < 768) {
+      this.navigationDrawer.mode = 'push'; // push or over
+      this.navigationDrawer.opened = false;
+    } else {
+      this.navigationDrawer.mode = 'side';
+      this.navigationDrawer.opened = true;
+    }
+  }
+
+  constructor() {
+    // Set side bar mode
+    this.screenWidth = window.innerWidth;
+    window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+      this.setSideBar();
+    };
+  }
+
+  ngAfterViewInit() {
+    this.setSideBar();
+  }
 }
