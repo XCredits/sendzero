e490a037e91e28d1d92e8a78dd02032acf712efa
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-04-20 11:36:39 +1000

    More component clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,6 @@ declare var require: any
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-let io = require('socket.io-client');
-let Peer = require('simple-peer');
-let SimpleSignalClient = require('simple-signal-client');
-let chunkSize = 60000
-// import SimpleSignalClient from 'simple-signal-client';
-// let socket = io();
-// import Peer from 'simple-peer';
-
-// let signalClient = new SimpleSignalClient(socket);
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
