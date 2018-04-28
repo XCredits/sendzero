c0eb8ceeb2448e62a9c44e6628fc22f85d62810a
James Jansson <jamesjansson@gmail.com>
2018-04-28 18:36:05 +1000

    Start authentication controller

---
+++ server/controllers/authentication.controller.js
@@ -0,0 +1,14 @@
+var User = require('../models/user.model.js');
+
+module.exports = function (app) {
+  app.post('/register', register);
+  app.post('/login', login);
+}
+
+function register() {
+
+}
+
+function login() {
+
+}
