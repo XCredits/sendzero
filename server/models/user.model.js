1eb5b82b92c0b363a97ae86b4384ce532276c292
James Jansson <jamesjansson@gmail.com>
2018-04-30 01:25:36 +1000

    Install bcrypt

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Promise = require("bluebird");
 mongoose.Promise = global.Promise;
 var Schema = mongoose.Schema;
-var crypto = require('crypto');
+var bcrypt = require('brcypt');
 
 var UserSchema = new Schema({
     givenName: {type: String},
