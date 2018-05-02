66a222cd0937dc48cf95d9d223347c7b25271bfc
James Jansson <jamesjansson@gmail.com>
2018-05-03 01:57:52 +1000

    Remove console.log

--- server/mongoose-start.js
+++ server/mongoose-start.js
@@ -3,9 +3,6 @@ var Promise = require("bluebird");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-console.log("mongoose.connection");
-console.log(mongoose.connection);
-
 mongoose.Promise = Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
