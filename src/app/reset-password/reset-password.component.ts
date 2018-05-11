af239f13dad820e88a4d9b1a42a4da94fc7d32a9
James Jansson <jamesjansson@gmail.com>
2018-05-11 23:23:55 +1000

    Add reset password component

---
+++ src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss']
+})
+export class ResetPasswordComponent implements OnInit {
+  form: FormGroup;
+  submitSuccess = false;
+  waiting = false;
+  formErrorMessage: string;
+
+  constructor( private http: HttpClient,
+    private userService: UserService ) { }
+
+  ngOnInit() {
+    this.form = new FormGroup ({
+      username: new FormControl('', [<any>Validators.required]),
+    });
+  }
+
+  submit(formData) {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.get('username').value);
+    // Clear state from previous submissions
+    this.formErrorMessage = undefined;
+
+    this.waiting = true;
+    this.http.post('/api/user/reset-password', {
+          'password': formData.password,
+        })
+        .subscribe(data => {
+          this.waiting = false;
+          this.submitSuccess = true;
+        },
+        errorResponse => {
+          this.waiting = false;
+          this.formErrorMessage = 'There was a problem submitting the form.';
+        });
+  }
+}
