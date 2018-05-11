72c34169a5d5e0197482eb2ab678aedcfdca7ad2
James Jansson <jamesjansson@gmail.com>
2018-05-12 03:11:30 +1000

    First pass at getting authorisation to work

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import * as jwtDecode from 'jwt-decode';
@@ -14,14 +15,29 @@ export class ResetPasswordComponent implements OnInit {
   submitSuccess = false;
   waiting = false;
   formErrorMessage: string;
+  username: string;
+  jwt: any;
 
   constructor( private http: HttpClient,
-    private userService: UserService ) { }
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute) {
+      activatedRoute.queryParamMap
+          .subscribe(paramMap => {
+            this.username = paramMap.params.username;
+            try {
+              this.jwt = jwtDecode(paramMap.params.auth);
+            } catch {
+              this.formErrorMessage = 'Link does not work (authorisation string not valid).';
+            }
+          });
+  }
 
   ngOnInit() {
     this.form = new FormGroup ({
-      username: new FormControl('', [<any>Validators.required]),
+      password: new FormControl('', [<any>Validators.required]),
     });
+    // get queryParamMap: Observable<ParamMap>
+    // https://angular.io/api/router/ActivatedRoute
     console.log('Attempting decode');
     console.log(jwtDecode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1YWVkNTM3NGQwMzQ1ZDJiZDZjN2M5NTkiLCJqdGkiOiJkZTZmNDUwOWEzODliZTQ2IiwidXNlcm5hbWUiOiJhMSIsInhzcmYiOiIzMTBmMzNmZDhhOTA0YWYxIiwiZXhwIjoxNTI1NzgyNjE2LCJpYXQiOjE1MjU1MDI4MzZ9.uQ_IIfkNISFuc4-EU9g4Zdu1Fp3luh50TVPNu3dy1gE'));
   }
