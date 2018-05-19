de940d783824c74d8c5abecbc669aca7991206e4
James Jansson <jamesjansson@gmail.com>
2018-05-19 23:43:30 +1000

    Clean up

--- server/controllers/mailing-list.controller.js
+++ server/controllers/mailing-list.controller.js
@@ -3,17 +3,7 @@ const MailingList = require('../models/mailing-list.model.js');
 var Promise = require("bluebird");
 
 module.exports = function (app) {
-  app.post('/api/join-mailing-list', 
-    viewRequest,
-    validateJoinMailingList,
-    viewRequest, 
-    joinMailingList, 
-    viewRequest);
-}
-
-function viewRequest(req, res, next){
-  console.log("Got in");
-  next();
+  app.post('/api/join-mailing-list', validateJoinMailingList, joinMailingList);
 }
 
 // /api/join-mailing-list
@@ -24,20 +14,16 @@ let validateJoinMailingList = [
 ];
 
 function joinMailingList(req, res) {
-  console.log("Successfully called backend route");
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.mapped() });
   }
-  console.log('Passed validation');
   let mailingListUser = new MailingList();
   mailingListUser.email = req.body.email;
   mailingListUser.givenName = req.body.givenName;
   mailingListUser.familyName = req.body.familyName;
-  console.log('Attempt to save');
   return mailingListUser.save()
       .then((result) => {
-        console.log('Successful call');
         res.status(200).send({ message: "Success" });
       })
       .catch((error) => {
