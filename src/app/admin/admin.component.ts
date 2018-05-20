968cafe925a1a9992d3cc989ee679d1a5c514e65
James Jansson <jamesjansson@gmail.com>
2018-05-20 23:47:21 +1000

    Move get information to the chart

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-admin',
@@ -8,24 +6,10 @@ import {MatSnackBar} from '@angular/material';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  mailingListCount: number;
 
-  constructor(
-      private http: HttpClient,
-      private snackBar: MatSnackBar,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.http.get<any>('/api/admin/mailing-list-count')
-        .subscribe(data => {
-          this.mailingListCount = data.count;
-        },
-        () => {
-          this.snackBar.open('Network error', 'Dismiss', {
-            duration: 5000
-          });
-        }
-        );
   }
 
 }
--- src/app/mailing-list-chart/mailing-list-chart.component.ts
+++ src/app/mailing-list-chart/mailing-list-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-mailing-list-chart',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mailing-list-chart.component.scss']
 })
 export class MailingListChartComponent implements OnInit {
+  mailingListCount: number;
 
-  constructor() { }
+  constructor(
+      private http: HttpClient,
+      private snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit() {
+    this.http.get<any>('/api/admin/mailing-list-count')
+    .subscribe(data => {
+      this.mailingListCount = data.count;
+    },
+    () => {
+      this.snackBar.open('Network error', 'Dismiss', {
+        duration: 5000
+      });
+    }
+    );
   }
 
 }
