88f64dfdfdb56910633c19e94cff8956f56e3b83
James Jansson <jamesjansson@gmail.com>
2018-05-22 17:46:07 +1000

    Create simple stats caller

--- server/controllers/stats.controller.js
+++ server/controllers/stats.controller.js
@@ -1,8 +1,10 @@
 const { check, validationResult } = require('express-validator/check');
 const MailingListStats = require('../models/mailing-list-stats.model.js');
 const statsService = require('../services/stats.service.js');
-var Promise = require('bluebird');
+const Promise = require('bluebird');
 const auth = require('../config/jwt-auth.js');
+
+const MailingList = require('../models/mailing-list.model.js');
 
 module.exports = function (app) {
   app.post('/api/admin/mailing-list-count', auth.jwt, auth.isAdmin, 
@@ -22,59 +24,12 @@ function mailingListCount(req, res) {
 
 
 function mailingListStatsReport(req, res) {
-  console.log('req.body');
-  console.log(req.body);
-  let maxStatsReturned = 1000;
-
-  // if (!req.body.scale) {
-    
-  // } else {
-  //   let generateScale = function ({scale, start, end}) {
-  //     const standardReturn = 20;
-  //     let stepSizeMs;
-  //     switch (scale) {
-  //       case 'seconds':
-  //         stepSizeMs = 1000;
-  //         break;
-  //       case 'minutes':
-  //         stepSizeMs = 1000 * 60;
-  //         break;
-  //       case 'hours':
-  //         stepSizeMs = 1000 * 60 * 60;
-  //         break;
-  //       case 'days':
-  //         stepSizeMs = 1000 * 60 * 60 * 24;
-  //         break;
-  //       case 'weeks':
-  //         stepSizeMs = 1000 * 60 * 60 * 24 *7;
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //     if (!start, !end) {
-  //       // ceil date now
-  //     }
-  //     // Floor
-
-
-  //   };
-
-
-  //   req.body.scale
-  //   MailingList.aggregate([
-  //     {
-  //       $project: {
-  //         "timeSubscribe": {
-  //           "$range" : [Date.now()-40*24*60*60*1000, Date.now(), 24*60*60*1000]
-  //         }
-  //       }
-  //     }])
-  //     .then(data => {
-  //       console.log(data);
-  //     });
-  //     // count:sum(1)
-  // }
-
-
-  // "Rest stops": { $range: [ 0, "$distance", 25 ] }
+  MailingListStats.find({})
+      .then(results => {
+        res.send(results);
+      })
+      .catch(err => {
+        res.status(500)
+            .send({message: 'Error retrieving data from stats database'});
+      });
 }
