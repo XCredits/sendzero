04f420b00bac39fa624b1a8d97e35338ac1d3d16
James Jansson <jamesjansson@gmail.com>
2018-05-23 11:59:56 +1000

    Start email service template

---
+++ server/services/email.service.js
@@ -0,0 +1,11 @@
+// Here you should put the external email service you require
+
+exports = {
+  addUserToMailingList,
+  removeUserFromMailingList,
+  retrieveUnsubscribes,
+  sendWelcomeEmail,
+  sendMailingListEmail,
+  sendPasswordResetEmail,
+  sendUsernameRetrievalEmail
+};
