ce38dba3f22c1516b3c20a3c45f04a82728fd23a
James Jansson <jamesjansson@gmail.com>
2018-05-23 17:42:37 +1000

    Fix SendGrid implementation

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -2,18 +2,21 @@
 
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-console.log("###########################################################");
-sgMail.send(msg);
 
+// #SENDGRID_VERIFICATION START
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com',
+//   from: 'test@example.com',
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg)
+//     .then(() => {console.log('SendGrid worked!')})
+//     .catch((err) => {console.log('Error occurred.'); console.log(err); });
+// #SENDGRID_VERIFICATION END
 
 exports = {
   addUserToMailingList,
