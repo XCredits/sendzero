76ed3f864a933ebc34605dbe65c0550aacefacc2
James Jansson <jamesjansson@gmail.com>
2018-05-23 14:01:17 +1000

    Start to structure basic email format

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -4,8 +4,48 @@ exports = {
   addUserToMailingList,
   removeUserFromMailingList,
   retrieveUnsubscribes,
-  sendWelcomeEmail,
-  sendMailingListEmail,
-  sendPasswordResetEmail,
-  sendUsernameRetrievalEmail
+  sendRegisterWelcome,
+  sendMailingListWelcome,
+  sendPasswordReset,
+  sendUsernameRetrieval,
 };
+
+/**
+ * Add the user to the mailing list in the email service provider
+ */
+function addUserToMailingList({userId, email, givenName, familyName}) {
+  
+}
+
+/**
+ * Remove the user from the mailing list in the email service provider
+ */
+function removeUserFromMailingList({userId, email}) {
+  
+}
+
+/**
+ * In this function, we are calling on the email service to retrieve instances 
+ * of the user pressing unsubscribe. We are doing this to compare to the user 
+ * list in our own databases, to remove individuals who have unsubscribed.
+ */
+function retrieveUnsubscribes({start, end}) {
+  
+}
+
+function sendRegisterWelcome({userId, email, givenName, familyName}) {
+  
+}
+
+function sendMailingListWelcome({userId, email, givenName, familyName}) {
+  
+}
+
+function sendPasswordReset({userId, email, givenName, familyName, resetLink}) {
+  
+}
+
+function sendUsernameRetrieval({userId, email, givenName, familyName, userNameArr}) {
+  
+}
+
