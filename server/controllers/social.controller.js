58c0239574e413b495a0cb49a711daa145f3a426
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-06-09 18:19:43 +1000

    Update social controller

--- server/controllers/social.controller.js
+++ server/controllers/social.controller.js
@@ -1,3 +1,4 @@
+'use strict';
 // The social controller intercepts scrapers and serves new URLs
 
 const socialLinks = require('./social-links.controller');
@@ -5,18 +6,21 @@ const socialLinks = require('./social-links.controller');
 const socialDefaults = {
   url: process.env.URL_ORIGIN,
   title: process.env.SITE_NAME,
-  description: '',
+  description: process.env.SITE_NAME,
   image: '/assets/img/social-default.jpg',
 };
 
 module.exports = function(req, res, next) {
-  const ua =req.headers['user-agent'];
-  if (ua.includes('FacebookExternalHit') ||
-      ua.includes('Facebot') ||
-      ua.includes('LinkedInBot') ||
-      ua.includes('Twitterbot') ) {
-    const customLink = socialLinks(req.path);
-    return res.send(html(customLink ? customLink : socialDefaults));
+  let ua = req.headers['user-agent'];
+  if (typeof ua === 'string') {
+    ua = ua.toLowerCase();
+    if (ua.includes('facebookexternalhit') ||
+        ua.includes('facebot') ||
+        ua.includes('linkedinbot') ||
+        ua.includes('twitterbot') ) {
+      const customLink = socialLinks(req.path);
+      return res.send(html(customLink ? customLink : socialDefaults));
+    }
   }
   next();
 };
@@ -27,19 +31,44 @@ module.exports = function(req, res, next) {
  * @return {*}
  */
 function html(v) {
+  let origin = process.env.URL_ORIGIN;
+  let twitterProperty = '';
+  let facebookProperty = '';
+  if (process.env.TWITTER_HANDLE) {
+    twitterProperty = `
+    <meta name="twitter:site" content="@${process.env.TWITTER_HANDLE}" />
+    <meta name="twitter:creator" content="@${process.env.TWITTER_HANDLE}" />`;
+  }
+  if (process.env.FACEBOOK_APP_ID) {
+    facebookProperty =
+        `<meta property="fb:app_id" content="${process.env.FACEBOOK_APP_ID}">`;
+  }
   return `
     <html>
       <head>
+        <meta charset="UTF-8" />
+        <meta name="author", content="${process.env.SITE_NAME}" />
+
+        ${facebookProperty}
         <meta property="og:url" content="${v.url}" />
         <meta property="og:type" content="article" />
         <meta property="og:title" content="${v.title}" />
         <meta property="og:description" content="${v.description}" />
-        <meta property="og:image" content="${v.image}" />
+        <meta property="og:image" content="${origin}${v.image}" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        ${twitterProperty}
+        <meta name="twitter:title" content="${v.title}" />
+        <meta name="twitter:description" content="${v.description}" />
+        <meta name="twitter:image" content="${origin}${v.image}" />
+
+        <meta name="description" content="${v.title}" />
+
       </head>
       <body>
         <h1>${v.title}</h1>
         <p>${v.description}</p>
-        <img src="${v.image}">
+        <img src="${origin}${v.image}">
       </body>
       
     </html>
