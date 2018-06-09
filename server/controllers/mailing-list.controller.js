ed952af0417d6266b2d521c9a2b61d9f702434a1
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-06-09 18:20:19 +1000

    Update mailing list controller

--- server/controllers/mailing-list.controller.js
+++ server/controllers/mailing-list.controller.js
@@ -1,5 +1,6 @@
 const validator = require('validator');
 const MailingList = require('../models/mailing-list.model.js');
+const emailService = require('../services/email.service.js');
 const MailingListStats = require('../models/mailing-list-stats.model.js');
 const statsService = require('../services/stats.service.js');
 // const Promise = require('bluebird');
@@ -35,7 +36,26 @@ function joinMailingList(req, res) {
   return mailingListUser.save()
       .then((result) => {
         res.status(200).send({message: 'Success'});
-        return statsService.increment(MailingListStats);
+        return statsService.increment(MailingListStats)
+            .catch((err)=>{
+              console.log('Error in the stats service');
+            });
+      })
+      .then(()=>{
+        return emailService.addUserToMailingList({
+              givenName, familyName, email,
+            })
+            .catch((err)=>{
+              console.log('Error in the mailing list service');
+            });
+      })
+      .then(()=>{
+        return emailService.sendMailingListWelcome({
+              givenName, familyName, email,
+            })
+            .catch((err)=>{
+              console.log('Error in the send email service');
+            });
       })
       .catch((error) => {
         console.log('Error');
