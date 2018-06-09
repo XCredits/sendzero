3ad0dafb7db8379d2f30b0991c41c2536eedec9d
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-06-09 18:32:44 +1000

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,36 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const socialController = require('./server/controllers/social.controller');
+const routes = require('./server/routes');
 
 app.use(bodyParser.urlencoded({extended: true})); // extended gives full JSON
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const routes = require('./server/routes');
+
+app.use('/', function(req, res, next) {
+  let redirect = false;
+  // Redirect non-https
+  if (req.headers['x-forwarded-proto'] === 'http' && // if using http
+      req.hostname !== 'localhost') { // & not a local install
+    if (req.method === 'GET') { // if it is a GET request
+      redirect = true;
+    } else { // if it is a POST, DELETE etc
+      // Throw an error
+      return res.status(400)
+          .send({message: 'Do not make API requests using http, use https'});
+    }
+  }
+  // Remove www
+  let strippedHostname = req.hostname;
+  if (strippedHostname.startsWith('www.')) {
+    strippedHostname = strippedHostname.slice(4);
+  }
+  if (redirect) {
+    return res.redirect('https://' + strippedHostname + req.originalUrl);
+  }
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
