f48cdd2515287999a046df91d9aa38ce62efb3d3
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-08-10 15:08:46 +1000

    Fix QR styling

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -1,20 +1,27 @@
 // https://github.com/Cordobo/angularx-qrcode
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { SendZeroService } from '../send-zero.service';
 
 @Component({
     selector: 'app-qr',
     templateUrl: './qrcode.component.html',
-    styleUrls: ['./qrcode.component.scss']
+    styleUrls: ['./qrcode.component.scss'],
 })
 
-export class QRCodeComponent implements OnInit {
+export class QRCodeComponent implements OnInit, AfterViewInit {
     @Input() link: string = null;
     constructor (private sendZeroService: SendZeroService) {
         this.link = sendZeroService.connectionLink;
     }
 
     ngOnInit() {
+    }
 
+    ngAfterViewInit() {
+        const qrDiv = document.getElementById('qr');
+        console.log(qrDiv);
+        const qrDivChild = qrDiv.firstElementChild;
+        const img = qrDivChild.lastElementChild;
+        img.setAttribute('width', '100%');
     }
 }
