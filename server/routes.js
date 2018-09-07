2e0ad45d6075b4f563126150a5a3a86308eb8fa5
Siddhant Virmani <siddhantvirmani@gmail.com>
2018-09-07 15:30:42 +1000

    Add route

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,13 @@ const authenticationController =
     require('./controllers/authentication.controller.js');
 const statsController =
     require('./controllers/stats.controller.js');
+const fileController =
+    require('./controllers/file.controller.js');
 
 module.exports = function(app) {
   mailingListController(app);
   authenticationController(app);
   statsController(app);
+  fileController(app);
 };
 
